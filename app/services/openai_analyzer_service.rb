8246a0c56febef7dd71bed5d9d08a6d145d3c595
Jonathan Ferreira <jonathan.vferreira1@gmail.com>
2025-05-30 12:34:58 -0300

    change model

--- app/services/openai_analyzer_service.rb
+++ app/services/openai_analyzer_service.rb
@@ -9,15 +9,23 @@ class OpenaiAnalyzerService
     client = OpenAI::Client.new
     response = client.chat(
       parameters: {
-        model: "gpt-4-0125-preview",
+        model: "gpt-4.1-2025-04-14",
         messages: [
           {
             role: "system",
-            content: "Analyze smartphone listings. Return JSON only: {\"chance_danificado\": 0-100, \"marca\": \"brand\", \"modelo\": \"model\", \"confiabilidade_dano\": 0-100, \"confiabilidade_marca\": 0-100, \"confiabilidade_modelo\": 0-100}."
+            content: "Analyze smartphone listings in detail. Return JSON only with the following structure: {
+              \"chance_danificado\": 0-100,
+              \"detalhes_dano\": \"detailed description of potential damage or condition\",
+              \"marca\": \"brand\",
+              \"modelo\": \"model\",
+              \"confiabilidade_dano\": 0-100,
+              \"confiabilidade_marca\": 0-100,
+              \"confiabilidade_modelo\": 0-100,
+            }"
           },
           {
             role: "user",
-            content: "Me diga qual a chance do aparelho celular estar danificado, qual a marca e o modelo. Dê um grau de confiabilidade para cada uma dessas informações usando uma escala de 0 a 100% dentro de um json para o seguinte anúncio: #{@url}"
+            content: "Analise detalhadamente essa foto de celular. Identifique a chance do aparelho estar danificado, marca e modelo. Inclua detalhes sobre o estado. Dê um grau de confiabilidade para as informações principais usando uma escala de 0 a 100% dentro de um json para o image nessa url: #{@url}"
           }
         ],
         temperature: 0.3,
