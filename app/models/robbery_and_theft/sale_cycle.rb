740acbddf7c9c6b25fc53e77676271a817403b6e
Jonathan Ferreira <jonathan.vferreira1@gmail.com>
2025-07-22 11:35:29 -0300

    change final step

--- app/models/robbery_and_theft/sale_cycle.rb
+++ app/models/robbery_and_theft/sale_cycle.rb
@@ -1,6 +1,8 @@
 class RobberyAndTheft::SaleCycle < SaleCycle
   default_scope { where(kind: 'robbery_and_theft') }
 
+  validates :imei, presence: true, if: :imei_info?
+
   def step_availables
     %w[send_validation_link access_validation_link imei_info waiting_review]
   end
